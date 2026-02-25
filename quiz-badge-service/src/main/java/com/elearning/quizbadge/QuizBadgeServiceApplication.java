7364dea3b2acc4d3dd2a9634abc1020d31aeccd4
Rahali <alrahalimohamed3@gmail.com>
2026-02-25 17:11:58 +0100

    Integrate latest local Spring microservices updates into rahali branch

--- quiz-badge-service/src/main/java/com/elearning/quizbadge/QuizBadgeServiceApplication.java
+++ quiz-badge-service/src/main/java/com/elearning/quizbadge/QuizBadgeServiceApplication.java
@@ -2,12 +2,14 @@ package com.elearning.quizbadge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class for Quiz Badge Service.
  * Provides quiz and badge functionality for the e-learning platform.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class QuizBadgeServiceApplication {
     
     public static void main(String[] args) {
